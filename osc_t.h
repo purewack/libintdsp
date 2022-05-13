614142a2cf0460a87e1ff5084075514e2784683f
purewack <bobimaster15@gmail.com>
2022-05-13 19:47:42 +0100

    osc freq setter fix

--- osc_t.h
+++ osc_t.h
@@ -13,4 +13,4 @@ struct osc_t{
 node_t* new_osc(agraph_t* graph, char* signature);
 void proc_osc(void* processor);
 //f_10 = desired freq * 10, i.e. provide 503 for 50.3hz, 200,005 for 20.5kHz etc
-void set_osc_freq(osc_t* processor, uint32_t f_10);
+void set_osc_freq(osc_t* processor, uint32_t f_10, uint32_t srate);
